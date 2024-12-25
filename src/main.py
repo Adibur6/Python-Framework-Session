312b56290b59aa689c5ee76ec5f84c5dc15fa760
Nazmul Ahsan <ahsan.shihab2@gmail.com>
2024-12-25 14:27:26 +0600

    add security dependency
    added security dependencies on the whole rooms router
    If you check the generated docs, you will see a lock icon.
    check the Security and Advanced Security section in the fastapi
    documentation for more information

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from src.routes.users import router as user_router
+from src.routes.rooms import router as room_router
 
 
 app = FastAPI()
 app.include_router(user_router)
-
+app.include_router(room_router)
