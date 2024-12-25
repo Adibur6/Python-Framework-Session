5dc4b33bb085cb2ba223f6edc268fd71d1649c2f
Nazmul Ahsan <ahsan.shihab2@gmail.com>
2024-12-25 14:03:50 +0600

    move id, created_at column to Base class
    We can assume that id, created_at columns are common
    for all our tables. So instead of repeating them for each table class
    definations, we can declare them in the Base class and when the Base class
    is inherited, the columns will be automatically added

--- src/models/base.py
+++ src/models/base.py
@@ -1,7 +1,10 @@
-
+from datetime import datetime, timezone
+from uuid import uuid4
+from sqlalchemy.orm import mapped_column, Mapped
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import String, DateTime
 
 
 db_url = "sqlite+aiosqlite:///mydb.db"
@@ -10,6 +13,5 @@ engine = create_async_engine(db_url, echo=True)
 SessionAsync = async_sessionmaker(engine, expire_on_commit=False)
 
 class Base(AsyncAttrs, DeclarativeBase):
-    pass
-
-
+    id: Mapped[str] = mapped_column(String(36), primary_key=True, default=lambda: str(uuid4()))
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=lambda: datetime.now(timezone.utc))
--- src/models/user.py
+++ src/models/user.py
@@ -1,19 +1,12 @@
-from datetime import datetime, timezone
-from uuid import uuid4
 from sqlalchemy.orm import mapped_column, Mapped
-from sqlalchemy import String, DateTime
-
+from sqlalchemy import String
 
 from .base import Base
 
 class User(Base):
     __tablename__ = "users"
 
-    id: Mapped[str] = mapped_column(String(36), primary_key=True, default=lambda: str(uuid4()))
     first_name: Mapped[str] = mapped_column(String(50), nullable=False)
     last_name: Mapped[str] = mapped_column(String(50), nullable=False)
     email: Mapped[str] = mapped_column(String(50), nullable=False)
     password_hash: Mapped[str] = mapped_column(String(100), nullable=False)
-
-    created_at: Mapped[datetime] = mapped_column(DateTime, default=lambda: datetime.now(timezone.utc))
-
